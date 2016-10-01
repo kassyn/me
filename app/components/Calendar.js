3de2639366a5c009382e8abb6183c101e6f5faf0
kassyn <kassyntec@gmail.com>
2016-10-01 18:03:52 -0300

    add module moment

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import moment from 'moment';
 import '../stylesheets/components/calendar.scss';
 
 class Calendar extends React.Component {
 	render() {
 		return (
 			<div>
-				<DatePicker inline locale='pt-br' />
+				<DatePicker
+					inline
+					selected={moment('2016-09-30')}
+					locale='pt-br'
+				/>
 			</div>
 		);
 	}
